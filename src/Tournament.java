f1a6ff05bd3744af68ffda6e8158c12db47f4d06
andreas svensson <andsveweb@gmail.com>
2024-04-10 16:38:23 +0200

    Number of fields Using regex to split between commas. printing the file and showing the fields that not have five parts in red.

--- src/Tournament.java
+++ src/Tournament.java
@@ -6,6 +6,11 @@ public class Tournament {
         try(BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
             String line;
             while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                if (parts.length !=5) {
+                    System.err.println("testing number of fields error" + line);
+                    continue;
+                }
                 System.out.println(line);
             }
         } catch (Exception e) {
